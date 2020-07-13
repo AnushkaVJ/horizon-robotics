2d304221c0424a383e85e5415065a49c8ab95584
AnushkaVJ <44513988+AnushkaVJ@users.noreply.github.com>
2020-07-13 21:08:07 +0530

    WP2Static CREATE app/themes/e25-base-theme/src/js/core/_data-isotope.js

---
+++ app/themes/e25-base-theme/src/js/core/_data-isotope.js
@@ -0,0 +1,34 @@
+var Isotope = require('isotope-layout');
+// require('isotope-cells-by-row');
+// https://isotope.metafizzy.co/extras.html
+
+$(() => {
+
+  if ($('.grid').length > 0 && $('[data-filter]').length > 0) {
+    const $buttonGroup = $('.filter-button-group');
+    const $filters = $('[data-filter]');
+    const iso = new Isotope('.grid', {
+      itemSelector: '.grid-item',
+      filter: '.asia'
+    });
+
+    $filters.on('click', (e) => {
+      iso.arrange({
+        filter: e.target.getAttribute('data-filter')
+      });
+    });
+
+    // set initial is-checked class
+    $('[data-filter=".asia"]').addClass('is-active');
+
+    // change is-checked class on buttons
+    $filters.each(function () {
+      $buttonGroup.on('click', 'a', function () {
+        $buttonGroup.find('.is-active').removeClass('is-active');
+        $(this).addClass('is-active');
+      });
+    });
+
+  }
+
+});
