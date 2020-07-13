76d8151469f6f96e7465c38830aab1a851836ef9
AnushkaVJ <44513988+AnushkaVJ@users.noreply.github.com>
2020-07-13 21:09:12 +0530

    WP2Static CREATE app/themes/e25-base-theme/src/js/main.js

---
+++ app/themes/e25-base-theme/src/js/main.js
@@ -0,0 +1,20 @@
+// CORE
+import './core/core-helpers';
+import './core/_data-slick';
+// import './core/_data-fix-on-scroll';
+import './core/_data-equal-height';
+import './core/_data-counterup';
+// import './core/_data-isotope';
+// import './core/_bs-background';
+// import './core/_bs-date-picker';
+
+// CUSTOM
+import './custom/_footer';
+import './custom/_autocomplete';
+import './custom/_equal-height';
+import './custom/_navigation';
+import './custom/_main-banner';
+import './custom/_tab-slider';
+import './custom/_slick-video';
+import './custom/_collapse';
+import './custom/_page-reload';
