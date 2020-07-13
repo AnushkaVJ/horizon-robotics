8fd5871370af701fbc3d23d6165aa45fb8ff49a0
AnushkaVJ <44513988+AnushkaVJ@users.noreply.github.com>
2020-07-13 21:01:47 +0530

    WP2Static CREATE app/themes/e25-base-theme/framework-customizations/extensions/post-blocks/shortcodes/single-post/static/js/scripts.js

---
+++ app/themes/e25-base-theme/framework-customizations/extensions/post-blocks/shortcodes/single-post/static/js/scripts.js
@@ -0,0 +1,10 @@
+// $(function(){
+//     var $grid = $('.grid').isotope({
+//         // options
+//     });
+//     // filter items on button click
+//     $('.filter-button-group').on( 'click', 'button', function() {
+//         var filterValue = $(this).attr('data-filter');
+//         $grid.isotope({ filter: filterValue });
+//     });
+// });
